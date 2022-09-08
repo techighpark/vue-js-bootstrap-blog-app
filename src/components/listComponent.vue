bc6ee7a680ca719038a0e765ccd121d89919c267
techighpark <techigh.b@gmail.com>
2022-09-08 11:03:46 +0900

    vue-router

---
+++ src/components/listComponent.vue
@@ -0,0 +1,21 @@
+<template lang="">
+  <div>
+    <h5>{{ title }}</h5>
+    <span>{{ date }}</span>
+    <div>
+      <p>{{ content }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ListComponent',
+  props: {
+    title: String,
+    content: String,
+    date: String,
+    number: Number,
+  },
+};
+</script>
+<style lang=""></style>
